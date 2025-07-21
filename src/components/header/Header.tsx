67a922241ba41b609514171aea145de28a3c92a6
samdeveloper23 <sam.developer.23@gmail.com>
2025-07-21 08:36:08 +0200

    correccion Link imagen header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -5,14 +5,15 @@ import {
   NavbarLink,
   NavbarToggle,
 } from "flowbite-react";
+import logo from "../../../../public/MiLogo.png";
 
 const Header = () => {
   return (
     <MegaMenu>
       <NavbarBrand href="/">
         <img
-          alt=""
-          src="../../public/MiLogo.png"
+          alt="pinturas eredraug"
+          src={logo}
           className="mr-3 h-6 rounded sm:h-9"
         />
         <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
