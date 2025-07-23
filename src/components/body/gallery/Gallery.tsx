c0d0456f555c417beefe41e3dd614d03f5240197
samdeveloper23 <sam.developer.23@gmail.com>
2025-07-23 09:15:53 +0200

    primeras incorporaciones

--- src/components/body/gallery/Gallery.tsx
+++ src/components/body/gallery/Gallery.tsx
@@ -1,9 +1,10 @@
 import logo from "../../../../public/MiLogo.png";
+import imagenPrimera from "../../../../public/antes-despues-pintura.png";
 
 const Gallery = () => {
   return (
     <>
-      <img src={logo} alt="" />
+      <img src={imagenPrimera} alt="" />
       <img src={logo} alt="" />
       <img src={logo} alt="" />
       <img src={logo} alt="" />
--- src/components/body/home/Home.tsx
+++ src/components/body/home/Home.tsx
@@ -1,5 +1,6 @@
 import { Card } from "flowbite-react";
 import logo from "../../../../public/MiLogo.png";
+import imagenPrimera from "../../../../public/antes-despues-pintura.png";
 
 const Home = () => {
   return (
@@ -8,17 +9,14 @@ const Home = () => {
         <Card
           className="max-w-sm"
           imgAlt="Meaningful alt text for an image that is not purely decorative"
-          imgSrc={logo}
+          imgSrc={imagenPrimera}
         >
           <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptate
-            modi quas placeat
+            Pintado de habitaciones desde 250€
           </h5>
           <p className="font-normal text-gray-700 dark:text-gray-400">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestias
-            consequatur voluptate ipsum ex velit eius hic, praesentium
-            dignissimos a. Laudantium culpa dolorem maiores itaque porro
-            officiis quidem voluptate quibusdam id.
+            Tratamiento de la pared "antimanchas, mohoo, salitres, etc" y
+            pintado, presupuesto sin compromiso.
           </p>
         </Card>
 
